51d621da52c0d50eb5ac943bfa8563ac758e8b7d
sudhanshu1711 <sudhanshu2679@gmail.com>
2025-09-06 15:12:48 +0530

    created the header component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,12 +14,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
         <body className={`${inter.className}`}>
         <ThemeProvider attribute="class" defaultTheme="dark">
-        {children}
+          <Header />
+       <main className="min-h-screen">{children}</main>
+        <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by Sudhanshu</p>
+              </div>
+            </footer>
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
